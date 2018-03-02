add7e43ae0c44f47c20e3b9537251bbdc585d4d6
Jamie Quinn <jjquinn42@gmail.com>
2018-03-02 20:37:31 +0000

    Added Mozilla's template webgl page

---
+++ main.js
@@ -0,0 +1,21 @@
+main();
+
+//
+// start here
+//
+function main() {
+  const canvas = document.querySelector("#glCanvas");
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+
+  // Only continue if WebGL is available and working
+  if (!gl) {
+    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+    return;
+  }
+
+  // Set clear color to green, fully opaque
+  gl.clearColor(0.0, 0.5, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
