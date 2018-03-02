d09b28bd58d9c9cceca5d4b3474a33f50724e315
Jamie Quinn <jjquinn42@gmail.com>
2018-03-02 22:39:25 +0000

    Removed pointless test function and shaders from main file

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// Vertex shader
-const vsSource = `
-  attribute vec4 aVertexPosition;
-  attribute vec2 aTextureCoord;
-
-  varying highp vec2 vTextureCoord;
-
-  void main(void) {
-    gl_Position = aVertexPosition;
-    vTextureCoord = aTextureCoord;
-  }
-`;
-
-// Fragment shader
-const fsSource = `
-  varying highp vec2 vTextureCoord;
-
-  uniform sampler2D uSampler;
-
-  void main(void) {
-    gl_FragColor = texture2D(uSampler, vTextureCoord);
-  }
-`;
-
 function main() {
   const canvas = document.querySelector("#glCanvas");
   // Initialize the GL context
--- utility.js
+++ utility.js
@@ -170,34 +170,6 @@ function loadTexture(gl, width_in, height_in) {
   return texture;
 }
 
-function loadBlueTexture(gl, width_in, height_in) {
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-
-  // Because images have to be download over the internet
-  // they might take a moment until they are ready.
-  // Until then put a single pixel in the texture so we can
-  // use it immediately. When the image has finished downloading
-  // we'll update the texture with the contents of the image.
-  const level = 0;
-  const internalFormat = gl.RGBA;
-  const width = width_in;
-  const height = height_in;
-  const border = 0;
-  const srcFormat = gl.RGBA;
-  const srcType = gl.UNSIGNED_BYTE;
-  const pixel = new Uint8Array([0, 0, 255, 255]);  // opaque blue
-  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
-                width, height, border, srcFormat, srcType,
-                pixel);
-
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-
-  return texture;
-}
-
 function loadScreenProgramInfo(gl) {
   // Vertex shader
   const vsSource = `
