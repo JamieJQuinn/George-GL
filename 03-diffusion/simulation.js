fa1d55f9d8ddd51934c40470c464591696d6c1d9
Jamie Quinn <jjquinn42@gmail.com>
2018-07-25 16:28:00 +0100

    Added diffusion

---
+++ 03-diffusion/simulation.js
@@ -0,0 +1,116 @@
+// Setup simulation program
+function loadSimInfo(gl) {
+  // Vertex shader
+  const vsSource = `#version 300 es
+    in vec4 aVertexPosition;
+    in vec2 aTextureCoord;
+
+    out vec2 vTextureCoord;
+
+    void main(void) {
+      gl_Position = aVertexPosition;
+      vTextureCoord = aTextureCoord;
+    }
+  `;
+
+  // Fragment shader
+  const fsSource = `#version 300 es
+    precision highp float;
+    in vec2 vTextureCoord;
+
+    uniform sampler2D uSampler;
+    uniform float time;
+    uniform float dt;
+    uniform vec2 dxy;
+
+    out vec4 outColour;
+
+    void main(void) {
+      // Get variables
+      vec2 u = texture(uSampler, vTextureCoord).xy;
+      vec2 umx = texture(uSampler, vec2(vTextureCoord.x - dxy.x, vTextureCoord.y)).xy;
+      vec2 upx = texture(uSampler, vec2(vTextureCoord.x + dxy.x, vTextureCoord.y)).xy;
+
+      // Constants
+      float sigma = 0.5;
+
+      // Perform numerical calculation
+      u.x = u.x + sigma * (upx.x - 2.0*u.x + umx.x);
+
+      // Output results
+      float alpha = texture(uSampler, vTextureCoord).w;
+      outColour = vec4(u, 0, alpha);
+    }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const info = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+    uniformLocations: {
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
+      time: gl.getUniformLocation(shaderProgram, 'time'),
+      dt: gl.getUniformLocation(shaderProgram, 'dt'),
+      dxy: gl.getUniformLocation(shaderProgram, 'dxy'),
+    },
+  };
+
+  setupVAO(gl, info);
+
+  return info;
+}
+
+// Main rendering/simulation loop
+function loop(gl, framebuffers, textures, screenInfo, simInfo, boundaryInfo, simVars) {
+  var count = simVars.count;
+  var time = simVars.time;
+
+  // Setup GL rendering
+  gl.useProgram(simInfo.program);
+  gl.bindVertexArray(simInfo.vao);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffers[(1 + count) % 2]);
+  bindTextureAsSampler(gl, simInfo, textures[(0 + count) % 2]);
+
+  // Load in uniforms
+  gl.uniform1f(simInfo.uniformLocations.time, time/1000);
+  gl.uniform1f(simInfo.uniformLocations.dt, 0.9/gl.canvas.width);
+  gl.uniform2f(simInfo.uniformLocations.dxy, 1.0/gl.canvas.width, 1.0/gl.canvas.height);
+
+  // Run one cycle of simulation
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+
+  applyBoundaryConditions(gl, framebuffers[(1 + count) % 2], textures[0 + (count % 2)], simInfo, boundaryInfo);
+
+  drawToScreen(gl, screenInfo, textures[count%2]);
+
+  simVars.timePrev = time;
+  simVars.count++;
+
+  setTimeout(function () {
+    window.requestAnimationFrame(function (tFrame) {
+      simVars.time = tFrame;
+      loop(gl, framebuffers, textures, screenInfo, simInfo, boundaryInfo, simVars);
+    });
+  }, 1000/30);
+}
+
+function startSimulation (gl) {
+  surfaces = createRenderingSurfaces(gl);
+
+  loadInitialConditions(gl, surfaces.framebuffers[0]);
+
+  const simInfo = loadSimInfo(gl);
+  const boundaryInfo = loadBoundaryInfo(gl);
+  const screenInfo = loadScreenInfo(gl);
+
+  var simVars = {
+    time: 0,
+    timePrev: 0,
+    count: 0,
+  };
+
+  loop(gl, surfaces.framebuffers, surfaces.textures, screenInfo, simInfo, boundaryInfo, simVars)
+}
---
+++ 03-diffusion/utility.js
@@ -0,0 +1,207 @@
+function bindTextureAsSampler(gl, programInfo, texture) {
+  // Tell WebGL we want to affect texture unit 0
+  gl.activeTexture(gl.TEXTURE0);
+
+  // Bind the texture to texture unit 0
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  // Tell the shader we bound the texture to texture unit 0
+  gl.uniform1i(programInfo.uniformLocations.uSampler, 0);
+}
+
+// Sets up vao object to render a simple screen-sized square
+function setupVAO(gl, info) {
+  info.vao = gl.createVertexArray();
+  gl.bindVertexArray(info.vao);
+
+  const num = 2;  // pull out 2 values per iteration
+  const type = gl.FLOAT;    // the data in the buffer is 32bit floats
+  const normalize = false;  // don't normalize
+  const stride = 0;         // how many bytes to get from one set of values to the next
+                            // 0 = use type and numComponents above
+  const offset = 0;         // how many bytes inside the buffer to start from
+
+  // Create and bind position buffer
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  const positions = [
+     1.0,  1.0,
+    -1.0,  1.0,
+     1.0, -1.0,
+    -1.0, -1.0,
+  ];
+  gl.bufferData(gl.ARRAY_BUFFER,
+                new Float32Array(positions),
+                gl.STATIC_DRAW);
+
+  gl.vertexAttribPointer(
+    info.attribLocations.vertexPosition, num, type, normalize, stride, offset);
+  gl.enableVertexAttribArray(info.attribLocations.vertexPosition);
+
+  if(info.attribLocations.textureCoord != -1) {
+    // Create and bind texture buffer
+    const textureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
+    const textureCoordinates = [
+      // Main Square
+      1.0,  1.0,
+      0.0,  1.0,
+      1.0,  0.0,
+      0.0,  0.0,
+    ];
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoordinates),
+                  gl.STATIC_DRAW);
+    gl.vertexAttribPointer(
+      info.attribLocations.textureCoord, num, type, normalize, stride, offset);
+    gl.enableVertexAttribArray(info.attribLocations.textureCoord);
+  }
+}
+
+function initShaderProgram(gl, vsSource, fsSource) {
+  // Initialize a shader program, so WebGL knows how to draw our data
+  const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+  const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+
+  // Create the shader program
+  const shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  // If creating the shader program failed, alert
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+    return null;
+  }
+
+  return shaderProgram;
+}
+
+function loadShader(gl, type, source) {
+  // creates a shader of the given type, uploads the source and compiles it
+  const shader = gl.createShader(type);
+
+  // Send the source to the shader object
+  gl.shaderSource(shader, source);
+
+  // Compile the shader program
+  gl.compileShader(shader);
+
+  // See if it compiled successfully
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+    return null;
+  }
+
+  return shader;
+}
+
+// Creates a texture with our required config for simulation
+function createTexture(gl, width_in, height_in) {
+  const texture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  const level = 0;
+  const internalFormat = gl.RGBA32I;
+  const width = width_in;
+  const height = height_in;
+  const border = 0;
+  const srcFormat = gl.RGBA_INTEGER;
+  const srcType = gl.INT;
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                null);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  return texture;
+}
+
+function createRenderingSurfaces(gl) {
+  var textures = [];
+  var framebuffers = [];
+
+  for (var ii = 0; ii < 2; ++ii) {
+    var texture = createTexture(gl, gl.canvas.width, gl.canvas.height);
+    textures.push(texture);
+
+    // Create a framebuffer
+    var fbo = gl.createFramebuffer();
+    framebuffers.push(fbo);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    // Attach a texture to it.
+    var attachmentPoint = gl.COLOR_ATTACHMENT0;
+    gl.framebufferTexture2D(
+            gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, texture, 0
+    );
+  }
+
+  return {
+    textures: textures,
+    framebuffers: framebuffers
+  }
+}
+
+// Loads shader for rendering simulation texture to screen
+function loadScreenInfo(gl) {
+  // Vertex shader
+  const vsSource = `#version 300 es
+
+    in vec4 aVertexPosition;
+    in vec2 aTextureCoord;
+
+    out vec2 vTextureCoord;
+
+    void main(void) {
+      gl_Position = aVertexPosition;
+      vTextureCoord = aTextureCoord;
+    }
+  `;
+
+  // Fragment shader
+  const fsSource = `#version 300 es
+
+    precision mediump float;
+
+    in vec2 vTextureCoord;
+
+    uniform sampler2D uSampler;
+
+    out vec4 outColour;
+
+    void main(void) {
+      vec4 grey = vec4(51.0/255.0, 51.0/255.0, 51.0/255.0, 1);
+      float ink = texture(uSampler, vTextureCoord).x;
+      outColour = ink*vec4(70.0/255.0,102.0/255.0,255.0/255.0,1) + (1.0-ink)*grey;
+    }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const info = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+    uniformLocations: {
+      uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
+    },
+  };
+
+  setupVAO(gl, info);
+
+  return info;
+}
+
+function drawToScreen(gl, screenInfo, texture) {
+  gl.useProgram(screenInfo.program);
+  gl.bindVertexArray(screenInfo.vao);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+  bindTextureAsSampler(gl, screenInfo, texture);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
