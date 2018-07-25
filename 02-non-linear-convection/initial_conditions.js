a87170184b31c8a224030a77503a6dd8a0b0b6cb
Jamie Quinn <jjquinn42@gmail.com>
2018-07-25 16:09:22 +0100

    Fixed naming error, nonlinear convection lession 01 should have been linear convection

---
+++ 02-non-linear-convection/initial_conditions.js
@@ -0,0 +1,58 @@
+function loadInitialProgramInfo(gl) {
+  // Vertex shader
+  const vsSource = `#version 300 es
+    in vec4 aVertexPosition;
+    in vec2 aTextureCoord;
+
+    out vec2 vTextureCoord;
+
+    void main(void) {
+      gl_Position = aVertexPosition;
+      vTextureCoord = aTextureCoord;
+    }
+  `;
+
+  // Fragment shader
+  const fsSource = `#version 300 es
+    precision mediump float;
+    in vec2 vTextureCoord;
+
+    out vec4 outColour;
+
+    const float PI = 3.1415926535897932384626433832795;
+
+    void main(void) {
+      vec2 pos = vTextureCoord.xy;
+      if(pos.x > 0.1 && pos.x < 0.3) {
+        outColour = vec4(1.0, 0, 1, 0.0);
+      } else {
+        outColour = vec4(1.0, 0, 0, 0.0);
+      }
+    }
+  `;
+
+  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+  const info = {
+    program: shaderProgram,
+    attribLocations: {
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+      textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
+    },
+  };
+
+  setupVAO(gl, info);
+
+  return info;
+}
+
+function loadInitialConditions(gl, fb) {
+  // Load & use program
+  var info = loadInitialProgramInfo(gl);
+  gl.useProgram(info.program);
+  // Load & use vertex attrib
+  gl.bindVertexArray(info.vao);
+  // Bind correct framebuffer
+  gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
+  // Finally draw
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
